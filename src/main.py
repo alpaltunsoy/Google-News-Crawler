4f33989eca0735ee7efb973a59a50c8dab35036b
Alp Altunsoy <alposmanaltun@gmail.com>
2024-05-28 15:19:33 +0300

    Adding main function

--- src/main.py
+++ src/main.py
@@ -3,34 +3,38 @@ from bs4 import BeautifulSoup
 
 
 
-#Downloading website
-webRequest = requests.get("https://news.google.com/home?hl=tr&gl=TR&ceid=TR:tr") # taking websites with requests
-print("Connecting to Google news...")
-webRequest.raise_for_status() #if conenction is failed it will raise an error
-webParsing = BeautifulSoup(webRequest.text, "lxml")
-print("Connection is successfull")
+def finding_categories(categories_dictionary):
 
-#lets take every topics
-all_topics = webParsing.find_all("a", class_="brSCsc")
+        #Downloading website
+        webRequest = requests.get("https://news.google.com/home?hl=tr&gl=TR&ceid=TR:tr") # taking websites with requests
+        print("Connecting to Google news...")
+        webRequest.raise_for_status() #if conenction is failed it will raise an error
+        webParsing = BeautifulSoup(webRequest.text, "lxml")
+        print("Connection is successfull")
 
-# if you dont want to topics write and exclude
-topics_exclude = ["Sizin için",
-                  "Takip edilenler",
-                  "Ana Sayfa",
-                  ]
-
-topics_dictionary = []
-
-#adding topics to the list
-for topics in all_topics:
-        if topics.get("aria-label") not in topics_exclude:
-                topics_dictionary.append({"Başlık":topics.get('aria-label'),"Link":topics.get("href")})
-                print(topics.get("aria-label"), "\n")
-                print(topics.get("href"), "\n")
-
-for elements in topics_dictionary:
-        print(elements)
-
+        #lets take every categories
+        all_categories = webParsing.find_all("a", class_="brSCsc")
 
         
-       
+        categories_exclude = [
+                        "Sizin için",
+                        "Takip edilenler",
+                        "Ana Sayfa",
+                                ]
+        #adding categories to the list
+        for categories in all_categories:
+                if categories.get("aria-label") not in categories_exclude:
+                        categories_dictionary.append({"Başlık":categories.get('aria-label'),"Link":categories.get("href")})
+
+def main():
+    
+    
+    categories_dictionary = [] #defining list for main_categories of news
+    finding_categories(categories_dictionary) #adding categories to our list 
+    
+
+
+
+if __name__ == "__main__":
+    main()
+        
