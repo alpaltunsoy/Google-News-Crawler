38f8584981ed3e74f329dd340e30d6cc64138d43
Alp Altunsoy <alposmanaltun@gmail.com>
2024-05-28 12:08:06 +0300

    Make a data more readable

--- src/main.py
+++ src/main.py
@@ -8,21 +8,28 @@ webRequest = requests.get("https://news.google.com/home?hl=tr&gl=TR&ceid=TR:tr")
 print("Connecting to Google news...")
 webRequest.raise_for_status() #if conenction is failed it will raise an error
 webParsing = BeautifulSoup(webRequest.text, "lxml")
-    
 print("Connection is successfull")
+
 #lets take every topics
 all_topics = webParsing.find_all("a", class_="brSCsc")
 
+# if you dont want to topics write and exclude
 topics_exclude = ["Sizin için",
                   "Takip edilenler",
                   "Ana Sayfa",
                   ]
 
+topics_dictionary = []
 
+#adding topics to the list
 for topics in all_topics:
         if topics.get("aria-label") not in topics_exclude:
+                topics_dictionary.append({"Başlık":topics.get('aria-label'),"Link":topics.get("href")})
                 print(topics.get("aria-label"), "\n")
                 print(topics.get("href"), "\n")
+
+for elements in topics_dictionary:
+        print(elements)
 
 
         
