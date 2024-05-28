9b7906b54726e4104491087e64da60ab769637d4
Alp Altunsoy <alposmanaltun@gmail.com>
2024-05-28 16:17:56 +0300

    some changes

--- src/main.py
+++ src/main.py
@@ -4,33 +4,71 @@ from bs4 import BeautifulSoup
 
 
 def finding_categories(categories_dictionary):
+        
+        
+                print("Connecting to Google news...")
+                try:
+                        webRequest = requests.get("https://news.google.com/home?hl=tr&gl=TR&ceid=TR:tr") # taking HTML of google news 
+                except:
+                        print("Program is closing. Link is not correct")
+                        exit()
 
-        #Downloading website
-        webRequest = requests.get("https://news.google.com/home?hl=tr&gl=TR&ceid=TR:tr") # taking websites with requests
-        print("Connecting to Google news...")
-        webRequest.raise_for_status() #if conenction is failed it will raise an error
-        webParsing = BeautifulSoup(webRequest.text, "lxml")
-        print("Connection is successfull")
+                #checking the status
+                if(webRequest.status_code == 200):
 
-        #lets take every categories
-        all_categories = webParsing.find_all("a", class_="brSCsc")
+                        print("Successfully connected to the Google News")
+                        webParsing = BeautifulSoup(webRequest.text, "lxml")
+        
+                        #lets take every categories
+                        all_categories = webParsing.find_all("a", class_="brSCsc")
+
+                        categories_exclude = [
+                                "Sizin için",
+                                "Takip edilenler",
+                                "Ana Sayfa",
+                                ]
+                        
+                        #adding categories to the list
+                        for categories in all_categories:
+                                if categories.get("aria-label") not in categories_exclude:
+                                        categories_dictionary.append({"Başlık":categories.get('aria-label'),"Link":categories.get("href")})
+                else:
+                        print("Failed to connect google news Try again!!!")
 
         
-        categories_exclude = [
-                        "Sizin için",
-                        "Takip edilenler",
-                        "Ana Sayfa",
-                                ]
-        #adding categories to the list
-        for categories in all_categories:
-                if categories.get("aria-label") not in categories_exclude:
-                        categories_dictionary.append({"Başlık":categories.get('aria-label'),"Link":categories.get("href")})
+        
+        
+
+
+def finding_news(categories_dictionary):
+      #Downloading website
+        
+       
+        url = "https://news.google.com"+ categories_dictionary[0]["Link"][1:]
+
+        try:
+                webRequestNews = requests.get(url) # taking websites with requests
+        except:
+                print("There are some problem on news side and closing program ")
+                exit()
+
+        if(webRequestNews.status_code == 200):
+                print("Finding news on " + categories_dictionary[0]["Başlık"]) 
+                
+      
+
+
 
 def main():
     
+
+        categories_dictionary = [] #defining list for main_categories of news
+        finding_categories(categories_dictionary) #adding categories to our list 
+        
+        finding_news(categories_dictionary)
+        
+
     
-    categories_dictionary = [] #defining list for main_categories of news
-    finding_categories(categories_dictionary) #adding categories to our list 
     
 
 
